e5e493f9a1ea30e006f51e076bc7db17d2e534cf
fxz <39674855+Iamfxz@users.noreply.github.com>
2022-03-13 16:29:33 +0800

    Update LP.py

--- code/LP.py
+++ code/LP.py
@@ -1,11 +1,12 @@
 from classes import Job, Jobshop
-import networkx as nx
+
 from pulp import *
 
 jobs = {}
 jobs[1] = Job(1, [1, 2, 3], [10, 8, 4])
 jobs[2] = Job(2, [2, 1, 4, 3], [8, 3, 5, 6])
 jobs[3] = Job(3, [1, 2, 4], [4, 7, 3])
+
 
 def LP(jobs):
     """
@@ -18,35 +19,35 @@ def LP(jobs):
 
     prob = LpProblem("Job shop", LpMinimize)
 
-    H = sum(js.nodes[j]['p'] for j in js)
+    H = sum(js.nodes[j]["p"] for j in js)
     T = range(H + 1)
 
     x = LpVariable.dicts("x", [(ij, t) for ij in js for t in T], 0, 1, cat=LpInteger)
 
-    C = LpVariable.dicts("C", [ij for ij in js])
+    ef = LpVariable.dicts("EF", [ij for ij in js])
     for ij in js:
-        prob += C[ij] == lpSum([t * x[(ij, t)] for t in T])
+        prob += ef[ij] == lpSum([t * x[(ij, t)] for t in T])
 
-    prob += C["V"]
+    prob += ef["V"]
 
     for ij in js:
         prob += lpSum([x[(ij, t)] for t in T]) == 1
 
     for ij in js:
-        prob += C[ij] >= js.nodes[ij]['p']
+        prob += ef[ij] >= js.nodes[ij]["p"]
 
     for ij in js:
-        for k in js. predecessors(ij):
-            prob += C[ij] >= C[k] + js.nodes[ij]['p']
+        for k in js.predecessors(ij):
+            prob += ef[ij] >= ef[k] + js.nodes[ij]["p"]
 
-    p = lambda ij, t: lpSum([x[(ij, u)] for u in range(t, t + js.nodes[ij]['p'])])
+    p = lambda ij, t: lpSum([x[(ij, u)] for u in range(t, t + js.nodes[ij]["p"])])
 
     for i in js.machines:
         for t in T:
-            prob += lpSum([p(ij, t) for ij in js.machines[i] if t <= H - js.nodes[ij]['p'] + 1]) <= 1
+            prob += lpSum([p(ij, t) for ij in js.machines[i] if t <= H - js.nodes[ij]["p"] + 1]) <= 1
 
     prob.solve(GUROBI())
-    #prob.solve()
+    # prob.solve()
 
     print("status", LpStatus[prob.status])
     print("objective", value(prob.objective))
@@ -54,8 +55,10 @@ def LP(jobs):
     for j in js:
         for t in T:
             if x[j, t].varValue > 0:
-                js.add_node(j, C=t)
+                js.add_node(j, EF=t)
 
     js.output()
 
-LP(jobs)
+
+if __name__ == "__main__":
+    LP(jobs)
